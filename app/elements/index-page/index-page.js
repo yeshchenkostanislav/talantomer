cd58a80e2a2ad27bf014d013fa9f72c17edc6df4
Stanislav Yeshchenko <yeshchenkostanislav@gmail.com>
2019-10-24 00:27:45 +0200

    first commit

---
+++ app/elements/index-page/index-page.js
@@ -0,0 +1,7 @@
+ var mySwiper = new Swiper('.swiper-container', {
+   loop: true,
+   navigation: {
+     nextEl: '.swiper-btn-next',
+     prevEl: '.swiper-button-prev',
+   }
+ })
---
+++ build/js/custom.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$('document').ready(function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-btn-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
+});
